bdc23c1bd55831acc9ddc8f7d532cdc27e8e4cb8
Mark Hanly <z3509778@192-168-1-112.tpgi.com.au>
2023-01-26 13:18:00 +1100

    changed r version for tests and removed code outside of test-randDAG

--- tests/testthat/test-randDAG.R
+++ tests/testthat/test-randDAG.R
@@ -1,10 +1,9 @@
-
-rd <- randDAG(4, .4)
-
 test_that("First element is a df", {
+  rd <- randDAG(4, .4)
   expect_equal(is.data.frame(rd$data), TRUE)
 })
 
 test_that("Second element is a dagitty", {
+  rd <- randDAG(4, .4)
   expect_equal(dagitty::is.dagitty(rd$dag), TRUE)
 })
